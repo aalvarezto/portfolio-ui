92f48b663b82e857f2f1072b07642af7ead5a1c9
Andres Alvarez <ndralvz@gmail.com>
2021-03-02 14:28:17 -0300

    Full template

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,26 +1,26 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
-  mount: {
-    /* ... */
-  },
-  plugins: [
-    /* ... */
-  ],
-  routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
-  ],
-  optimize: {
-    /* Example: Bundle your final build: */
-    // "bundle": true,
-  },
-  packageOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
-};
+	mount: {
+		// directory name: 'build directory'
+		public: "/",
+		src: "/dist",
+	},
+	plugins: ["@snowpack/plugin-react-refresh"],
+	routes: [
+		/* Enable an SPA Fallback in development: */
+		// {"match": "routes", "src": ".*", "dest": "/index.html"},
+	],
+	optimize: {
+		/* Example: Bundle your final build: */
+		// "bundle": true,
+	},
+	packageOptions: {
+		/* ... */
+	},
+	devOptions: {
+		/* ... */
+	},
+	buildOptions: {
+		/* ... */
+	},
+}
