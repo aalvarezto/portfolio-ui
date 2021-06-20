3b28d04a366ceb4d49704876bbe6b910a259dc73
andy <andy@DESKTOP-FJDBNI9.localdomain>
2021-06-19 23:29:33 -0300

    :sparkles: changed to definite menu header

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,54 @@
 "use strict"
 
-import React from "react"
+import React, { useState } from "react"
+import { Menu } from "antd"
+import {
+	MailOutlined,
+	AppstoreOutlined,
+	SettingOutlined,
+} from "@ant-design/icons"
 import "antd/dist/antd.css"
-import "./App.css"
-import { Layout, Menu, Breadcrumb } from "antd"
 
-const { Header, Content, Footer } = Layout
+const { SubMenu } = Menu
 
 const App = () => {
+	const [current, setCurrent] = useState("mail")
+
+	const handleClick = e => {
+		console.log("click ", e)
+		setCurrent(e.key)
+	}
+
 	return (
-		<Layout className="layout">
-			<Header>
-				<div className="logo" />
-				<Menu
-					theme="dark"
-					mode="horizontal"
-					defaultSelectedKeys={["2"]}>
-					{new Array(15).fill(null).map((_, index) => {
-						const key = index + 1
-						return <Menu.Item key={key}>{`nav ${key}`}</Menu.Item>
-					})}
-				</Menu>
-			</Header>
-			<Content style={{ padding: "0 50px" }}>
-				<Breadcrumb style={{ margin: "16px 0" }}>
-					<Breadcrumb.Item>Home</Breadcrumb.Item>
-					<Breadcrumb.Item>List</Breadcrumb.Item>
-					<Breadcrumb.Item>App</Breadcrumb.Item>
-				</Breadcrumb>
-				<div className="site-layout-content">Content</div>
-			</Content>
-			<Footer style={{ textAlign: "center" }}>
-				Ant Design ©2018 Created by Ant UED
-			</Footer>
-		</Layout>
+		<Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
+			<Menu.Item key="mail" icon={<MailOutlined />}>
+				Navigation One
+			</Menu.Item>
+			<Menu.Item key="app" disabled icon={<AppstoreOutlined />}>
+				Navigation Two
+			</Menu.Item>
+			<SubMenu
+				key="SubMenu"
+				icon={<SettingOutlined />}
+				title="Navigation Three - Submenu">
+				<Menu.ItemGroup title="Item 1">
+					<Menu.Item key="setting:1">Option 1</Menu.Item>
+					<Menu.Item key="setting:2">Option 2</Menu.Item>
+				</Menu.ItemGroup>
+				<Menu.ItemGroup title="Item 2">
+					<Menu.Item key="setting:3">Option 3</Menu.Item>
+					<Menu.Item key="setting:4">Option 4</Menu.Item>
+				</Menu.ItemGroup>
+			</SubMenu>
+			<Menu.Item key="alipay">
+				<a
+					href="https://ant.design"
+					target="_blank"
+					rel="noopener noreferrer">
+					Navigation Four - Link
+				</a>
+			</Menu.Item>
+		</Menu>
 	)
 }
 
